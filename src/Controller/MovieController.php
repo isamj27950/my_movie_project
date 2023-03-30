9a1072f48e04976ff2773d9f168e0f3124489d9b
Isabelle Maitre-Jean <isamj27950@gmail.com>
2023-03-31 00:20:38 +0200

    css index

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -12,8 +12,17 @@ class MovieController extends AbstractController
     #[Route('/movie', name: 'app_movie')]
     public function index(MovieRepository $repo): Response
     {
-      $posts= $repo->findAll();
-      return $this->render('movie/index.html.twig',compact('posts'));  
-      
+        $posts= $repo->findAll();
+        return $this->render('movie/index.html.twig',compact('posts'));  
     }
+
+    #[Route('/post/{id}',name: 'app_showmovie')]
+    public function show($id,MovieRepository $repo): Response
+    {
+        //je recupere le poste avec l'id
+        $post = $repo->find($id);
+        //je passe à la vue
+        return $this->render('movie/showmovie.html.twig', compact('post'));
+    }
+
 } 
