592f8086d967ed3baccaf8c92429db938016a4d1
Isabelle Maitre-Jean <isamj27950@gmail.com>
2023-03-31 13:55:13 +0200

    add validate

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -3,26 +3,80 @@
 namespace App\Form;
 
 use App\Entity\Movie;
+use phpDocumentor\Reflection\PseudoTypes\Numeric_;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MovieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('Author')
-            ->add('description')
+            ->add('title',TextType::class,[
+                'attr'  => [
+                    'class' =>'form',
+                ],
+                'label'=> 'Titre du film',
+                'constraints' => [
+                    new NotBlank([
+                        "message" =>"Champs obligatoire"
+                    ]),
+                    new Length([
+                        'min'=>3,
+                        'max'=>20,
+                        'minMessage'=>"Minimum {{ limit }}  caractéres" ,
+                        'maxMessage'=>"Maximum {{ limit }} caractéres",
+                    ])
+                ]
+            ])
+            ->add('Author',TextType::class,[
+                'attr'  => [
+                    'class' =>'form',
+                ],
+                'label'=> 'Réalisateur ',
+                'constraints' => [
+                    new NotBlank([
+                        "message" =>"Champs obligatoire"
+                    ]),
+                    new Length([
+                        'min'=>3,
+                        'max'=>20,
+                        'minMessage'=>"Minimum {{ limit }}  caractéres" ,
+                        'maxMessage'=>"Maximum {{ limit }} caractéres",
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class,[
+                'attr'  => [
+                    'class' =>'form',
+                ],
+                'label'=> 'Synopsis',
+                'constraints' => [
+                    new NotBlank([
+                        "message" =>"Champs obligatoire"
+                    ]),
+                    new Length([
+                        'min'=>10,
+                        'max'=>200,
+                        'minMessage'=>"Minimum {{ limit }}  caractéres" ,
+                        'maxMessage'=>"Maximum {{ limit }} caractéres",
+                    ])
+                ]
+            ])
             ->add('image',FileType::class,[
                 'mapped' => false,
                 'attr'  => [
                     'class' =>'form',
                 ],
-                'label'=> 'Image du post',
+                'label'=> 'Affiche',
                 'constraints'=> [
                     //new NotBlank([
                      //   "message" =>"Champs obligatoire"
@@ -37,8 +91,40 @@ class MovieFormType extends AbstractType
                     ])
                 ]
             ] )
-            ->add('product_year')
-            ->add('category')
+            ->add('product_year',NumberType::class,[
+                'attr'  => [
+                    'class' =>'form',
+                ],
+                'label'=> 'Date de sortie',
+                'constraints' => [
+                    new NotBlank([
+                        "message" =>"Champs obligatoire"
+                    ]),
+                    new Length([
+                        'min'=>3,
+                        'max'=>5,
+                        'minMessage'=>"Minimum {{ limit }}  caractéres" ,
+                        'maxMessage'=>"Maximum {{ limit }} caractéres",
+                    ])
+                ]
+            ])
+            ->add('category',TextType::class,[
+                'attr'  => [
+                    'class' =>'form',
+                ],
+                'label'=> 'Catégorie',
+                'constraints' => [
+                    new NotBlank([
+                        "message" =>"Champs obligatoire"
+                    ]),
+                    new Length([
+                        'min'=>3,
+                        'max'=>20,
+                        'minMessage'=>"Minimum {{ limit }}  caractéres" ,
+                        'maxMessage'=>"Maximum {{ limit }} caractéres",
+                    ])
+                ]
+            ])
         ;
     }
 
